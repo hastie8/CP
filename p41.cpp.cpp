605b6849ed5acd909a9e926eee3a64b260257dbe
hastie8 <117348761+hastie8@users.noreply.github.com>
2022-11-06 22:04:25 +0330

    Add files via upload

---
+++ p41.cpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+main() {
+	
+	float r,g,s,d,m,h;
+	cin>>r>>g;
+	
+	s=r+g;
+	d=r-g;
+	m=r*g;
+	h=r/g;
+	
+	cout<<"s:" << s <<"  "<<"d:"<< d <<"  "<<"m:"<< m <<"  "<<"h:"<< h; 
+	
+}
---
+++ p42.cpp.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+ main() {
+ 	
+ 	float x,y,z,s,d,f,m,n,a;
+ 	cin>>x>>y>>z;
+ 	s=(x-y);
+ 	d=(x+z);
+ 	f=s*d;
+ 	m=(x+y+z)*(x+y+z);
+ 	n=m+1;
+ 	a=f/n;
+ 	cout<<a;
+ 	
+ 	
+}
