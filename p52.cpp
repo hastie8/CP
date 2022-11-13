5d1f2cc4dc177d9c30a92153eccc8af9fbcaf38a
hastie8 <117348761+hastie8@users.noreply.github.com>
2022-11-13 10:28:17 +0330

    Add files via upload

---
+++ p52.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+ main() {
+ 	char word;
+ 	cin >> word ;
+	switch (word){
+		case 'a':
+			cout<< "VOWEL";
+			break;
+		case 'e':
+			cout<< "VOWEL";
+			break;
+		case 'i':
+			cout<< "VOWEL";
+			break;
+		case 'o':
+			cout<< "VOWEL";
+			break;	
+		case 'u':
+			cout<< "VOWEL";
+			break;		
+	}
+
+}
